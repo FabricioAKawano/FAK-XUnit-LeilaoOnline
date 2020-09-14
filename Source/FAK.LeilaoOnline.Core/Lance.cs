0a371a9c4691fe60ccfbe06ee6d7454f0fece59c
Fabricio Kawano <fapa_si@yahoo.com.brfapa_si@yahoo.com.br git config --global user.email fapa_si@yahoo.com.br>
2020-09-14 10:49:06 -0300

    Add project files.

---
+++ Source/FAK.LeilaoOnline.Core/Lance.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FAK.LeilaoOnline.Core
+{
+    public class Lance
+    {
+        private Interessada Cliente { get; }
+        private decimal Valor { get; }
+
+        public Lance(Interessada cliente, decimal valor)
+        {
+            Cliente = cliente;
+            Valor = valor;
+        }
+    }
+}
---
+++ Source/FAK.LeilaoOnline.Core/Leilao.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FAK.LeilaoOnline.Core
+{
+    public class Leilao
+    {
+        private IList<Lance> _lances;
+        public IEnumerable<Lance> Lances => _lances;
+        public string Peca { get;}
+
+        public Leilao(string peca)
+        {
+            Peca = peca;
+        }
+
+        public void RecebeLance(Interessada cliente, decimal valor)
+        {
+            _lances.Add(new Lance(cliente, valor));
+        }
+
+        public void IniciaPregao()
+        {
+
+        }
+
+        public void TerminaPregao()
+        {
+
+        }
+    }
+}
